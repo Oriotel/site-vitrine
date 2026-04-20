509073cf371f5a8ce9f1cdf603f7eba40ce07c43
Ossama Saidi <osm.saidi@gmail.com>
2026-04-20 12:18:41 +0100

    feat: avancement sur la home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import SponsorsMarquee from '@/components/home/SponsorsMarquee'
 import Hero from '@/components/home/Hero';
 import AboutSection from '@/components/home/AboutSection';
@@ -9,18 +8,11 @@ import EventsSection from '@/components/home/EventsSection';
 const HomePage = () => {
   return (
     <>
-      {/* 1. La section principale (Hero) */}
       <Hero />
-      {/* 2. LA NOUVELLE SECTION "QUI SOMMES-NOUS" */}
-      {/* Elle va glisser naturellement sur le Hero ! */}
       <AboutSection />
-      {/* Composant ExpertisesSectorielles uniquement */}
       <ExpertisesSection />
-      {/* Composant Événements avec effet Coverflow 3D */}
       <EventsSection />
-      {/* Composant Témoignages en exclusivité sur la page pour les tests */}
       <TestimonialsSection />
-      {/* 3. La bande défilante des sponsors en noir */}
       <SponsorsMarquee />
     </>
   )
